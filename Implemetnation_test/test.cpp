85f37a2442cdcbe1b84ff19fe2e375d7d28bbca4
OLIVER O.J <oliver_msn94@hotmail.com>
2021-05-20 10:47:10 +0200

    gjorde en Google Test/Unit Test

--- Implemetnation_test/test.cpp
+++ Implemetnation_test/test.cpp
@@ -1,22 +1,29 @@
 #include "pch.h"
-#include "this/package/foo.h"
-#include "gtest/gtest.h"
+#include "../Implementation_lib/GameObject.h"
+#include "../Implementation_lib/GameObject.cpp"
+
 
 namespace my {
 	namespace project {
 		namespace {
 
 			// The fixture for testing class Foo.
-			class FooTest : public ::testing::Test {
+			class GameObject : public ::testing::Test {
 			protected:
 				// You can remove any or all of the following functions if their bodies would
 				// be empty.
 
-				FooTest() {
+				GameObject()
+				{
 					// You can do set-up work for each test here.
+					interactionOptions = new std::string * [4]{ nullptr };
+					interactionOptions[0] = new std::string("Greet");
+					interactionOptions[1] = new std::string("Goodbye");
+					interactionOptions[2] = new std::string("Salute");
+					interactionOptions[3] = new std::string("Fight");
 				}
 
-				~FooTest() override {
+				~GameObject() override {
 					// You can do clean-up work that doesn't throw exceptions here.
 				}
 
@@ -26,6 +33,11 @@ namespace my {
 				void SetUp() override {
 					// Code here will be called immediately after the constructor (right
 					// before each test).
+					interactionOptions[0] = new std::string("Greet");
+					interactionOptions[1] = new std::string("Goodbye");
+					interactionOptions[2] = new std::string("Salute");
+					interactionOptions[3] = new std::string("Fight");
+
 				}
 
 				void TearDown() override {
@@ -33,22 +45,28 @@ namespace my {
 					// before the destructor).
 				}
 
+				
 				// Class members declared here can be used by all tests in the test suite
-				// for Foo.
+				std::string** interactionOptions;
 			};
 
-			// Tests that the Foo::Bar() method does Abc.
-			TEST_F(FooTest, MethodBarDoesAbc) {
-				const std::string input_filepath = "this/package/testdata/myinputfile.dat";
-				const std::string output_filepath = "this/package/testdata/myoutputfile.dat";
-				Foo f;
-				EXPECT_EQ(f.Bar(input_filepath, output_filepath), 0);
+			// Tests something
+			TEST_F(GameObject, gameObjTest)
+			{
+				std::string tempString;
+				for (int i = 0; i < 4; i++)
+				{
+					
+					tempString += std::to_string(i + 1);
+					tempString += ". ";
+					tempString += *interactionOptions[i];
+					tempString += "\n";
+
+				}
+				EXPECT_STRCASEEQ(tempString.c_str(), "1. Greet\n2. GoodBye\n3. Salute\n4. Fight\n");
 			}
 
-			// Tests that Foo does Xyz.
-			TEST_F(FooTest, DoesXyz) {
-				// Exercises the Xyz feature of Foo.
-			}
+			
 
 		}  // namespace
 	}  // namespace project
